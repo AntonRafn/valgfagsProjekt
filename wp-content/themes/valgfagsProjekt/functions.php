3ac147785164d14e9df33efcefd7e557a60c8a74
AntonRafn <10503104@ucn.dk>
2025-09-19 16:08:46 +0200

    Tilføjelse af opskrift custom post

--- wp-content/themes/valgfagsProjekt/functions.php
+++ wp-content/themes/valgfagsProjekt/functions.php
@@ -1,15 +1,31 @@
 <?php
 
 
-function create_brand_post_type() {
+function create_brand_post_type()
+{
     register_post_type('brand', array(
         'labels' => array(
             'name' => 'Brands',
             'singular_name' => 'Brand'
         ),
         'public' => true,
-        'has_archive' => false, 
+        'has_archive' => false,
         'rewrite' => array('slug' => 'brand'),
+        'supports' => array('title', 'editor', 'thumbnail')
+    ));
+}
+add_action('init', 'create_brand_post_type');
+
+function create_opskrift_post_type()
+{
+    register_post_type('opskrift', array(
+        'labels' => array(
+            'name' => 'Opskrifter',
+            'singular_name' => 'Opskrift'
+        ),
+        'public' => true,
+        'has_archive' => false,
+        'rewrite' => array('slug' => 'opskrift'),
         'supports' => array('title', 'editor', 'thumbnail')
     ));
 }
@@ -17,9 +33,10 @@ add_action('init', 'create_brand_post_type');
 
 
 
-function valgfagsProjekt_enqueue_styles() {
+function valgfagsProjekt_enqueue_styles()
+{
     wp_enqueue_style(
-        'valgfagsProjekt-style', 
+        'valgfagsProjekt-style',
         get_stylesheet_uri(),
         array(),
         filemtime(get_stylesheet_directory() . '/style.css')
@@ -31,7 +48,8 @@ add_action('wp_enqueue_scripts', 'valgfagsProjekt_enqueue_styles');
 
 
 
-function enqueue_font_awesome() {
+function enqueue_font_awesome()
+{
     wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css');
 }
 add_action('wp_enqueue_scripts', 'enqueue_font_awesome');
