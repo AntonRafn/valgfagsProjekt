4ddf6a70ea7e6b7291219534ef6531d65149ff19
AntonRafn <10503104@ucn.dk>
2025-09-19 10:21:50 +0200

    Tjek med header og footer
    Hopefully henter de enkelt stående sider header og footer korrekt, så vi undgår overflødig kode med f.eks nav.

---
+++ wp-content/themes/valgfagsProjekt/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    
+<nav>
+  <div class="navbar">
+    <img class="logo" src="./assetsen/images/logoen.jpg" alt="Logo" />
+
+    <div class="nav-links">
+      <div class="dropdown">
+        <button class="dropdownButton">
+          Chefs <i class="fa-solid fa-chevron-down"></i>
+        </button>
+        <div class="dropdownContent">
+          <a href="">Home chefs</a>
+          <a href="">Amateur chefs</a>
+          <a href="">Professional chefs</a>
+        </div>
+      </div>
+      <a href="">Recipes</a>
+      <a href="">Brands</a>
+      <a href="">Stories</a>
+
+    </div>
+
+    <div class="login">
+      <a href=""><i class="fa-solid fa-user"></i> Log in</a>
+    </div>
+  </div>
+</nav>
+<div class="spacer-wrapper">
--- wp-content/themes/valgfagsProjekt/page-brands.php
+++ wp-content/themes/valgfagsProjekt/page-brands.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <?php wp_head(); ?>
-</head>
-
-<body>
-    <div class="spacer-wrapper">
-    <div class="hero">
-    <img src="" alt="">
-    </div>
+<?php get_header(); ?>
 
 <div class="brands-intro">
     <h1>Brands We Trust</h1>
@@ -126,8 +113,7 @@
         </div>
 
     </div>
-</div>
-</body>
-</html>
 
 
+
+<?php get_footer(); ?>
