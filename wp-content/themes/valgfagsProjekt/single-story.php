2b9df78ec18a8cba9911f63211f4a133cb2ae6c1
AntonRafn <10503104@ucn.dk>
2025-09-22 03:51:19 +0200

    Update single-story.php

--- wp-content/themes/valgfagsProjekt/single-story.php
+++ wp-content/themes/valgfagsProjekt/single-story.php
@@ -1,26 +1,29 @@
 <?php get_header() ?>
 <main>
-    <?php while(have_posts()) {
-        the_post()?>
-      <section>
-        <div class="heroSection">
-          <img
-            src="<?php echo get_field('hero_image')['url'] ?>"
-            alt=""
-          />
-          <div class="welcome-text">
-            <h1><?php the_field('story_title') ?></h1>
-            <p>By <?php the_author(); ?></p>
-          </div>
-        </div>
-      </section>
-      <section>
-        <div class="storyIndhold">
-          <p>
-            <?php the_field('story_text') ?>
-          </p>
-        </div>
-      </section>
-        <?php } ?>
-    </main>
-    <?php get_footer() ?>
+    <?php while (have_posts()) {
+        the_post() ?>
+        <section>
+            <div class="heroSection">
+                <img
+                    src="<?php echo get_field('hero_image')['url'] ?>"
+                    alt="" />
+                <div class="welcome-text">
+                    <h1><?php the_field('story_title') ?></h1>
+                    <p>By <?php the_author(); ?></p>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="storyIndhold">
+                <p>
+                    <?php the_field('story_text') ?>
+                </p>
+            </div>
+        </section>
+    <?php } ?>
+
+    <div class="breadcrumb">
+        <a href="<?php echo get_post_type_archive_link('story'); ?>">Back to Stories</a>
+    </div>
+</main>
+<?php get_footer() ?>
