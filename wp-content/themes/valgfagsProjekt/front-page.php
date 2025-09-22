3bdbf182d45dfba130b8c8c27ebea33252ea8132
AntonRafn <10503104@ucn.dk>
2025-09-22 02:34:05 +0200

    Update front-page.php

--- wp-content/themes/valgfagsProjekt/front-page.php
+++ wp-content/themes/valgfagsProjekt/front-page.php
@@ -11,7 +11,7 @@
     </div>
   </section>
   <section>
-    <h2 class="overskrifter">Meet the chefs</h2>
+    <h2 class="overskrifter">Welcome to our new Chefs</h2>
     <div class="chefsSection">
       <?php
 
@@ -36,7 +36,7 @@
         $photo_url = $photo ? $photo['url'] : '';
       ?>
         <div class="rundeKokke">
-          <img src="<?php echo ($photo_url); ?>" alt="" />
+          <a href="<?php echo (get_author_posts_url($chef->ID)); ?>"><img src="<?php echo ($photo_url); ?>" alt="" /></a>
           <p><?php echo get_field('chef_name', 'user_' . $chef->ID); ?></p>
           <p><?php echo ($type); ?></p>
           <a href="<?php echo (get_author_posts_url($chef->ID)); ?>">See more <i class="fa-solid fa-chevron-right"></i></a>
@@ -46,61 +46,49 @@
     </div>
   </section>
   <section>
-    <h2 class="overskrifter">Top rated recipes</h2>
+    <h2 class="overskrifter">Newest recipes</h2>
     <div class="allCards">
+      <?php
+      $args = array(
+      'post_type' => 'opskrift',
+      'posts_per_page' => 3,
+      'orderby' => 'date',
+      'order' => 'DESC',
+);
+
+$recipes = new WP_Query($args);
+    while ($recipes->have_posts()){
+      $recipes->the_post();
+      
+        $chef_id = get_post_field('post_author', get_the_ID());
+        $chef_user = get_userdata($chef_id);
+        $chef_image = get_field('chef_photo', 'user_' . $chef_id);
+      ?>
+
+      
+
       <div class="lilleOpskriftCard">
         <div class="card-img">
-          <img class="chefIcon" src="./assetsen/images/joeM.jpg" alt="" />
+          <img class="chefIcon" src="<?php echo($chef_image)['url'] ?>" alt="" />
           <img
-            src="./assetsen/images/air_fryer_aubergine_96744_16x9.jpg"
+            src="<?php get_field('billede_af_ret')['url'] ?>"
             alt="" />
         </div>
         <div class="card-indhold">
-          <p>Air fryer aubergine parmigiana</p>
+          <p><?php the_field('titel_ret') ?></p>
 
           <div class="rating">
             <span class="stars">★★★★★</span>
-            <span class="score"> 3.8</span>
+            <span class="score"> <?php the_field('rating') ?></span>
             <span class="votes">| 8 ratings</span>
           </div>
         </div>
       </div>
-      <div class="lilleOpskriftCard">
-        <div class="card-img">
-          <img class="chefIcon" src="./assetsen/images/joeM.jpg" alt="" />
-          <img
-            src="./assetsen/images/air_fryer_aubergine_96744_16x9.jpg"
-            alt="" />
-        </div>
-        <div class="card-indhold">
-          <p>Air fryer aubergine parmigiana</p>
 
-          <div class="rating">
-            <span class="stars">★★★★★</span>
-            <span class="score"> 3.8</span>
-            <span class="votes">| 8 ratings</span>
-          </div>
-        </div>
-      </div>
-      <div class="lilleOpskriftCard">
-        <div class="card-img">
-          <img class="chefIcon" src="./assetsen/images/joeM.jpg" alt="" />
-          <img
-            src="./assetsen/images/air_fryer_aubergine_96744_16x9.jpg"
-            alt="" />
-        </div>
-        <div class="card-indhold">
-          <p>Air fryer aubergine parmigiana</p>
+    <?php } ?>
 
-          <div class="rating">
-            <span class="stars">★★★★★</span>
-            <span class="score"> 3.8</span>
-            <span class="votes">| 8 ratings</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+      
+      
 </main>
 
 <?php get_footer() ?>
