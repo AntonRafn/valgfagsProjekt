f7258ee10b87d36a5d5c3a2f3ca78ae2c4fa273f
AntonRafn <10503104@ucn.dk>
2025-09-22 06:40:25 +0200

    Update functions.php

--- wp-content/themes/valgfagsProjekt/functions.php
+++ wp-content/themes/valgfagsProjekt/functions.php
@@ -15,7 +15,7 @@ function create_brand_post_type()
         'has_archive' => true,
         'rewrite' => array('slug' => 'brands'),
         'supports' => array('title', 'editor', 'thumbnail', 'author', 'comments'),
-        'menu_icon' => 'dashicons-admin-multisite'
+        'menu_icon' => 'dashicons-admin-filter'
     ));
 }
 add_action('init', 'create_brand_post_type');
@@ -53,7 +53,7 @@ function create_story_post_type()
         'has_archive' => true,
         'rewrite' => array('slug' => 'stories'),
         'supports' => array('title', 'editor', 'thumbnail', 'author'),
-        'menu_icon' => 'dashicons-products'
+        'menu_icon' => 'dashicons-book'
     ));
 }
 add_action('init', 'create_story_post_type');
