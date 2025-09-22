0a22feffc82c691c601b4335e3260eb681b4d757
AntonRafn <10503104@ucn.dk>
2025-09-22 02:14:30 +0200

    Update footer.php

--- wp-content/themes/valgfagsProjekt/footer.php
+++ wp-content/themes/valgfagsProjekt/footer.php
@@ -1,12 +1,19 @@
 </div>
 <footer>
+  <?php 
+  $user = get_user_by('login', 'adminKunde');
+  $public_image = get_field('public_image', 'user_' . $user->ID);
+  $public_name = get_field('public_name', 'user_' . $user->ID);
+  $public_number = get_field('public_number', 'user_' . $user->ID);
+  $public_email = get_field('public_email', 'user_' . $user->ID);
+  ?>
   <div class="footerInfo">
     <div class="profilInfo">
-      <img src="./assetsen/images/MasterchefBossLady.jpg" alt="">
+      <img src="<?php echo($public_image) ?>" alt="">
       <div class="kundeInfo">
-        <p>Alice T.</p>
-        <p>Tel.: 99999999</p>
-        <p> Email: aliceT@gmail.com</p>
+        <p><?php echo($public_name) ?></p>
+        <p>Tel.: <?php echo($public_number) ?></p>
+        <p><?php echo($public_email) ?></p>
       </div>
     </div>
     <div class="copPri">
