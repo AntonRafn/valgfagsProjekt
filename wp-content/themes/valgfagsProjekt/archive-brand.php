991d70328de5a9e54ac754a61567d238fa48f9aa
AntonRafn <10503104@ucn.dk>
2025-09-22 09:09:10 +0200

    Update archive-brand.php

--- wp-content/themes/valgfagsProjekt/archive-brand.php
+++ wp-content/themes/valgfagsProjekt/archive-brand.php
@@ -1,24 +1,3 @@
-<?php get_header(); ?>
-
-
-<?php
-$archive_settings = get_page_by_path('archive-headings');
-
-$brands_hero = get_field('brands_hero_image', $archive_settings->ID);
-$brands_intro = get_field('brands_hero_heading', $archive_settings->ID);
-?>
-
-<section class="heroSection">
-  <img src="<?php echo ($brands_hero['url']); ?>" />
-  <div class="welcome-text">
-
-
-    <div class="brands-intro">
-      <h1>Brands We Trust</h1>
-      <p><?php echo ($brands_intro) ?></p>
-    </div>
-</section>
-
 <div class="brands-favourites">
   <h2 class="overskrifter">Newest Brands</h2>
 
@@ -52,37 +31,33 @@ $brands_intro = get_field('brands_hero_heading', $archive_settings->ID);
       </div>
 
     <?php } ?>
+  </div>
+</div>
 
 
+<div class="brands-favourites">
+  <h2 class="overskrifter">All Brands</h2>
 
-
-    <div class="brands-favourites">
-      <h2 class="overskrifter">All Brands</h2>
-
-      <div class="brands-grid">
-
-        <?php while (have_posts()) {
-          the_post() ?>
-          <div class="brand-card">
-            <div class="brand-logo">
-              <img src="<?php the_field('hero_image')['url'] ?>" alt="">
-            </div>
-            <div class="brand-name">
-              <h3><?php the_field('brand_name') ?></h3>
-            </div>
-            <div class="brand-description">
-              <p><?php echo wp_trim_words(get_field('brand_description'), 10, '...') ?></p>
-              <div class="rating">
-                <span class="stars">★★★★</span>
-                <span class="score"> 3.8</span>
-              </div>
-            </div>
+  <div class="brands-grid">
+    <?php while (have_posts()) {
+      the_post() ?>
+      <div class="brand-card">
+        <div class="brand-logo">
+          <img src="<?php the_field('hero_image')['url'] ?>" alt="">
+        </div>
+        <div class="brand-name">
+          <h3><?php the_field('brand_name') ?></h3>
+        </div>
+        <div class="brand-description">
+          <p><?php echo wp_trim_words(get_field('brand_description'), 10, '...') ?></p>
+          <div class="rating">
+            <span class="stars">★★★★</span>
+            <span class="score"> 3.8</span>
           </div>
-        <?php } ?>
-
+        </div>
       </div>
-    </div>
+    <?php } ?>
+  </div>
+</div>
 
-
-
-    <?php get_footer(); ?>
+<?php get_footer(); ?>
